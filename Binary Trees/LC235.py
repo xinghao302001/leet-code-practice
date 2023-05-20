85223e689073b2252b0baf85921370e7585ff46f
xinghao302001 <wxhlovelife@gmail.com>
2023-05-20 23:25:57 +0200

    update BinaryTree -> add lc235

---
+++ Binary Trees/LC235.py
@@ -0,0 +1,14 @@
+# Lowest Common Ancestor of a Binary Search Tree
+
+from NodeDefinition import TreeNode
+from typing import Optional, List
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
+        
