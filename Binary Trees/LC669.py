9fc83f4be344282a6d9cb0b6d29acb1b38b430ac
xinghao302001 <wxhlovelife@gmail.com>
2023-05-21 16:30:49 +0200

    update BinaryTree -> add lc669

---
+++ Binary Trees/LC669.py
@@ -0,0 +1,21 @@
+# Trim a Binary Search Tree
+
+
+from NodeDefinition import TreeNode
+from typing import List,Optional
+
+
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+
+        if not root: return None
+
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+
+        return root
