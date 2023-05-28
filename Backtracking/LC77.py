78dfa5592118e87bc3c15420e3740c127a6ebb70
xinghao302001 <wxhlovelife@gmail.com>
2023-05-28 12:24:57 +0200

    Update Backtracking --> add lc77

---
+++ Backtracking/LC77.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        res = []
+        path = []
+
+        def backtracking(n, k, startIndex):
+            
+            if (len(path) == k):
+                res.append(path[:])
+                return
+
+            for i in range(startIndex, n-(k-len(path))+2):
+                path.append(i)
+                backtracking(n, k, i+1)
+                path.pop()
+            
+        backtracking(n, k, 1)
+
+        return res
+
+
+# if __name__ == "__main__":
+#     n=4
+#     k=2
+#     sol = Solution()
+#     res = sol.combine(4,2)
+#     print(res)
+
+
