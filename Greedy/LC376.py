f3be10f52146f90742ccd78d2b1f5d376cddb49a
xinghao302001 <wxhlovelife@gmail.com>
2023-05-31 23:44:36 +0200

    update Greedy --> add lc53.py

---
+++ Greedy/LC376.py
@@ -0,0 +1,12 @@
+## Wiggle Subsequence
+
+from typing import List
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        preC, curC, res = 0, 0, 1
+        for i in range(len(nums)-1):
+            curC = nums[i+1] - nums[i]
+            if curC * preC <= 0 and curC != 0:
+                res += 1
+                preC = curC
+        return res
