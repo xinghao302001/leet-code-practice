1821428fb7f2affe009698ff2e667ccce77572f4
xinghao302001 <wxhlovelife@gmail.com>
2023-06-06 14:02:23 +0200

    update Greedy --> add lc738.py

--- Greedy/LC738.py
+++ Greedy/LC738.py
@@ -6,7 +6,7 @@ class Solution:
 
         for i in range(len(str_num)-1, 0 , -1):
             if int(str_num[i]) < int(str_num[i-1]):
-                str_num[i] = str(int(str_num[i-1]) - 1)
+                str_num[i-1] = str(int(str_num[i-1]) - 1)
                 ## change to "9": start from position i to end
                 str_num[i:] = '9' * (len(str_num) - i)
 
