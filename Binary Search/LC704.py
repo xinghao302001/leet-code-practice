38c4c10cf7bde9e3f5a91b2489fed51901ae1d2f
xinghao302001 <wxhlovelife@gmail.com>
2023-06-07 21:36:16 +0200

    update Binary Search --> add template for binary search

--- Binary Search/LC704.py
+++ Binary Search/LC704.py
@@ -1,7 +1,9 @@
+## Template for binary search
 
 from typing import List
 
 class Solution:
+    ## case 1: basic case ---> find target
     def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
@@ -14,5 +16,39 @@ class Solution:
                 right = mid - 1
             elif (nums[mid] < target):
                 left = mid + 1
-                
+
         return -1
+    
+    ## Case 2: find target and find its lower bound
+    def left_bound(nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while(left <= right):
+            mid = left + (right-left) // 2
+            if nums[mid] == target:
+                right = mid -1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif  nums[mid] < target:
+                left = mid + 1
+        if left == len(nums):
+            return -1
+        return left if nums[left] == target else -1
+
+    ## Case 3: find target and find its higher bound
+    def right_bound(nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while(left <= right):
+            mid = left + (right-left) // 2
+            if nums[mid] == target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif  nums[mid] < target:
+                left = mid + 1
+        if left - 1 < 0:
+            return -1
+        return left-1 if nums[left-1] == target else -1
