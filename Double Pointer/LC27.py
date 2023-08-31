69cbc177bbe4c791b13f1b479be0defb14ba41fd
xinghao302001 <wxhlovelife@gmail.com>
2023-08-31 18:45:18 +0200

    add Pointer LC27.py

---
+++ Double Pointer/LC27.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        fast = 0 
+        slow = 0
+        size = len(nums)
+        while fast < size:
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        
+        return slow
