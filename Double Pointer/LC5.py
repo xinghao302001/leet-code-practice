b820afcf8b673c593f78bcb266006e1bf74da95c
xinghao302001 <wxhlovelife@gmail.com>
2023-08-31 20:55:59 +0200

    add Double Pointer LC5.py

---
+++ Double Pointer/LC5.py
@@ -0,0 +1,35 @@
+from typing import List
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        def palindrome( s, l, r):
+            while(l >= 0 and r < len(s) and s[l]==s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
+        
+        res = ""
+        for i in range(len(s)):
+            s1 = palindrome(s, i, i)
+            s2 = palindrome(s, i, i+1)
+
+            res = res if len(res) > len(s1) else s1
+            res = res if len(res) > len(s2) else s2
+        return res
+
+
+
+
+
+'''
+    def is_palindrome(self, s, start, end) -> bool:
+        i =  start
+        j = end
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+
+        return True
+'''
