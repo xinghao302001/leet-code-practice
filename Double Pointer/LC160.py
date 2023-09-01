39c736c2eb7ea93a8d66d60d2bac55c9460eaa21
xinghao302001 <wxhlovelife@gmail.com>
2023-09-01 17:12:23 +0200

    add Double Pointer LC160.py

---
+++ Double Pointer/LC160.py
@@ -0,0 +1,23 @@
+### ListNode
+
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p1, p2 = headA, headB
+        while p1 != p2:
+            if p1 == None:
+                p1 = headB
+            else:
+                p1 = p1.next
+
+            if p2 == None:
+                p2 = headA
+            else:
+                p2 = p2.next
+        return p1
