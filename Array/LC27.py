52c071fd94207e6ae62ffee0676365fbcf5ac533
xinghao302001 <wxhlovelife@gmail.com>
2024-11-19 21:59:38 +0100

    : Fast and Slow Pointer

---
+++ Array/LC27.py
@@ -0,0 +1,14 @@
+## Fast and Slow pointer
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        fast = 0
+        slow = 0
+        while fast < len(nums):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
