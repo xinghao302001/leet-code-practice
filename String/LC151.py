b21ac58fb6aceae30020313a1757a52ff74e7feb
xinghao302001 <wxhlovelife@gmail.com>
2024-11-22 09:11:32 +0100

    :double pointer

---
+++ String/LC151.py
@@ -0,0 +1,13 @@
+## double pointer
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()
+        words = s.split()
+
+        left, right = 0, len(words) - 1
+        while left < right:
+            words[left], words[right] = words[right], words[left]
+            left += 1
+            right -= 1
+
+        return " ".join(words)
