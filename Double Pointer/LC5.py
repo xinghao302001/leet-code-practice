eb3595f675eeaff69d921314be597812e0f13e8b
xinghao302001 <wxhlovelife@gmail.com>
2024-12-20 12:06:12 +0100

    : string: double pointer/center diffusion

--- Double Pointer/LC5.py
+++ Double Pointer/LC5.py
@@ -1,27 +1,26 @@
+# Center Diffusion
 from typing import List
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        def palindrome( s, l, r):
-            while(l >= 0 and r < len(s) and s[l]==s[r]):
+        def palindrome(s, l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
-            return s[l+1:r]
-        
+            return s[l + 1 : r]
+
         res = ""
         for i in range(len(s)):
             s1 = palindrome(s, i, i)
-            s2 = palindrome(s, i, i+1)
+            s2 = palindrome(s, i, i + 1)
 
             res = res if len(res) > len(s1) else s1
             res = res if len(res) > len(s2) else s2
         return res
 
 
-
-
-
-'''
+"""
     def is_palindrome(self, s, start, end) -> bool:
         i =  start
         j = end
@@ -32,4 +31,4 @@ class Solution:
             j -= 1
 
         return True
-'''
+"""
