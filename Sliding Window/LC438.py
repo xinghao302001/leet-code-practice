f0d71967b8c08fc396199d5b4394c2c93db681a9
xinghao302001 <wxhlovelife@gmail.com>
2025-06-08 23:34:07 +0200

    : more efficient way for lc438

--- Sliding Window/LC438.py
+++ Sliding Window/LC438.py
@@ -1,41 +1,23 @@
 ## sliding Window
-from typing import List
-from collections import defaultdict
+from collections import Counter
 
 
 class Solution:
     ## t <=> p
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        need, window = defaultdict(int), defaultdict(int)
-
-        for c in p:
-            need[c] += 1
-
-        left, right = 0, 0
-        valid = 0
+    def findAnagrams(s: str, p: str):
         res = []
-
-        while right < len(s):
-            c = s[right]
-            right += 1
-
-            if c in need:
-                window[c] += 1
-                if window[c] == need[c]:
-                    valid += 1
-            # 判断左侧窗口是否要收缩
-            while right - left >= len(p):
-                # 当窗口符合条件时，把起始索引加入 res
-                if valid == len(need):
-                    res.append(left)
-                d = s[left]
+        p_count = Counter(p)
+        window_count = Counter()
+        left = 0
+        for right in range(len(s)):
+            window_count[s[right]] += 1
+            if right - left + 1 > len(p):
+                window_count[s[left]] -= 1
+                if window_count[s[left]] == 0:
+                    del window_count[s[left]]
                 left += 1
-
-                if d in need:
-                    if window[d] == need[d]:
-                        valid -= 1
-                    window[d] -= 1
-
+            if window_count == p_count:
+                res.append(left)
         return res
 
 
