41fbcba213b7ab5a0e8038caf89845cea3876ffd
xinghao302001 <wxhlovelife@gmail.com>
2025-06-11 17:44:52 +0200

    : small fixes

--- Greedy/LC56.py
+++ Greedy/LC56.py
@@ -3,22 +3,22 @@
 
 from typing import List
 
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) == 0: return intervals
-        intervals.sort(key=lambda x:x[0])
+        if len(intervals) == 0:
+            return intervals
+        intervals.sort(key=lambda x: x[0])
         result = []
 
         result.append(intervals[0])
-        
-        for i in range(1,len(intervals)):
+
+        for i in range(1, len(intervals)):
             last = result[-1]
-            ### !!! 
+            ### !!!
             if last[1] >= intervals[i][0]:
                 last[1] = max(intervals[i][1], last[1])
                 result[-1] = [last[0], last[1]]
             else:
                 result.append(intervals[i])
         return result
-    
-
