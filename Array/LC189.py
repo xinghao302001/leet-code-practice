787324007d31cf9f172cb4a5dccd1895e84679dc
xinghao302001 <wxhlovelife@gmail.com>
2025-06-12 20:04:20 +0200

    : Array-Hard

---
+++ Array/LC189.py
@@ -0,0 +1,21 @@
+## O(1)
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        k %= length
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        reverse(0, length - 1)
+        reverse(0, k - 1)
+        reverse(k, length - 1)
